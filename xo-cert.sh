ab78427a5b25ca1f7e93c73231a39c199202f0d5
Luke McConnell <gh-megabit@users.noreply.github.com>
2026-01-09 08:04:14 +0000

    Add script to manage SSL certificate for Xen Orchestra

---
+++ xo-cert.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/usr/bin/cp /etc/letsencrypt/live/xo.ecomltd.co.uk/fullchain.pem /srv/xen-orchestra-ce/xo-server/certificate.pem
+/usr/bin/cp /etc/letsencrypt/live/xo.ecomltd.co.uk/privkey.pem /srv/xen-orchestra-ce/xo-server/key.pem
+/usr/bin/docker-compose -f /srv/xen-orchestra-ce/docker-compose.yml restart
