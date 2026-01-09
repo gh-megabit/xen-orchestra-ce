3d64589cb0697fff6e0763a7f6a21d9e6e0618b7
root <root@xo.singlemode.co.uk>
2026-01-09 13:20:13 +0000

    Updated cert script for new docker compose syntax

--- xo-cert.sh
+++ xo-cert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /usr/bin/cp /etc/letsencrypt/live/xo.singlemode.co.uk/fullchain.pem /srv/xen-orchestra-ce/xo-server/certificate.pem
 /usr/bin/cp /etc/letsencrypt/live/xo.singlemode.co.uk/privkey.pem /srv/xen-orchestra-ce/xo-server/key.pem
-/usr/bin/docker-compose -f /srv/xen-orchestra-ce/docker-compose.yml restart
+/usr/bin/docker compose -f /srv/xen-orchestra-ce/docker-compose.yml restart
